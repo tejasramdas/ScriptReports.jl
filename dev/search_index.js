3a99837c18c1bf3bd0b77529b09a0364a9cdbbe9
Documenter.jl <documenter@juliadocs.github.io>
2025-02-23 20:09:37 +0000

    build based on d4ddc80

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ScriptReports","category":"page"},{"location":"#ScriptReports","page":"Home","title":"ScriptReports","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ScriptReports.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ScriptReports]","category":"page"}]
+}
